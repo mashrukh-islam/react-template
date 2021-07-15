9d0e14306e21929b89c7ff51104079fd70da2920
Mashrukh <mashrukh@wednesday.is>
2021-07-15 19:24:56 +0530

    Tests for tracks reducer
    Tests are written for the tracks reducer. Test cases are REQUEST_GET_TRACK_NAMES,
    SUCCESS_GET_TRACK_NAMES,CLEAR_TRACK_NAMES. All the test cases are passing.

--- app/containers/ITunesProvider/tests/reducer.test.js
+++ app/containers/ITunesProvider/tests/reducer.test.js
@@ -1,5 +1,5 @@
 // import produce from 'immer'
-import { iTunesProviderReducer, initialState } from '../reducer';
+import { iTunesProviderReducer, initialState, iTunesProviderTypes } from '../reducer';
 
 /* eslint-disable default-case, no-param-reassign */
 describe('ITunesProvider reducer tests', () => {
@@ -10,5 +10,36 @@ describe('ITunesProvider reducer tests', () => {
 
   it('should return the initial state', () => {
     expect(iTunesProviderReducer(undefined, {})).toEqual(state);
+  });
+
+  it('should return initial state when action of type REQUEST_GET_TRACK_NAMES is dispatched', () => {
+    const artistName = 'Opeth';
+    const expectedResult = { ...state, artistName };
+    expect(
+      iTunesProviderReducer(state, {
+        type: iTunesProviderTypes.REQUEST_GET_TRACK_NAMES,
+        artistName
+      })
+    ).toEqual(expectedResult);
+  });
+
+  it('should ensure track data is present when action of type SUCCESS_GET_TRACK_NAMES is dispatched', () => {
+    const data = { resultCount: 0, results: [] };
+    const expectedResult = { ...state, tracks: data };
+    expect(
+      iTunesProviderReducer(state, {
+        type: iTunesProviderTypes.SUCCESS_GET_TRACK_NAMES,
+        data
+      })
+    ).toEqual(expectedResult);
+  });
+
+  it('should return state when action of type CLEAR_TRACK_NAMES is dispatched', () => {
+    const expectedResult = { ...state };
+    expect(
+      iTunesProviderReducer(state, {
+        type: iTunesProviderTypes.CLEAR_TRACK_NAMES
+      })
+    ).toEqual(expectedResult);
   });
 });
