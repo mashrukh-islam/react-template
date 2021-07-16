db09b67236d122f2ba14e358726cb7e898fbe12b
Mashrukh <mashrukh@wednesday.is>
2021-07-16 19:23:39 +0530

    Completed tests for saga and selectors
    Tests are written for saga and selectors

--- app/containers/ITunesProvider/tests/saga.test.js
+++ app/containers/ITunesProvider/tests/saga.test.js
@@ -3,9 +3,67 @@
  */
 
 /* eslint-disable redux-saga/yield-effects */
+import { takeLatest, call, put, select } from 'redux-saga/effects';
+import { getTracks } from '@services/itunesApi';
+import { apiResponseGenerator } from '@utils/testUtils';
+import iTunesProviderSaga, { fetchTrackDetails, fetchTracks } from '../saga';
+import { iTunesProviderTypes } from '../reducer';
+import { selectTrackById } from '../selectors';
 
 describe('ITunesProvider saga tests', () => {
-  it('sample test', () => {
-    expect(1).toEqual(1);
+  const generator = iTunesProviderSaga();
+  const artistName = 'Opeth';
+  const trackId = 123456;
+  let getTracksGenerator = fetchTracks({ artistName });
+  let getTrackDetailsGenerator = fetchTrackDetails({ trackId });
+
+  it('should start task to watch for REQUEST_GET_TRACK_NAMES action', () => {
+    expect(generator.next().value).toEqual(takeLatest(iTunesProviderTypes.REQUEST_GET_TRACK_NAMES, fetchTracks));
+  });
+
+  it('should ensure that the action FAILURE_GET_TRACK_NAMES is dispatched when the api call fails', () => {
+    const res = getTracksGenerator.next().value;
+    expect(res).toEqual(call(getTracks, artistName));
+    const errorResponse = {
+      message: 'Internal Server Error'
+    };
+    expect(getTracksGenerator.next(apiResponseGenerator(false, errorResponse)).value).toEqual(
+      put({
+        type: iTunesProviderTypes.FAILURE_GET_TRACK_NAMES,
+        error: errorResponse
+      })
+    );
+  });
+
+  it('should ensure that the action SUCCESS_GET_TRACK_NAMES is dispatched when the api call succeeds', () => {
+    getTracksGenerator = fetchTracks({ artistName });
+    const res = getTracksGenerator.next().value;
+    expect(res).toEqual(call(getTracks, artistName));
+    const tracksResponse = {
+      resultCount: 1,
+      results: []
+    };
+    expect(getTracksGenerator.next(apiResponseGenerator(true, tracksResponse)).value).toEqual(
+      put({
+        type: iTunesProviderTypes.SUCCESS_GET_TRACK_NAMES,
+        data: tracksResponse
+      })
+    );
+  });
+
+  it('should ensure that the action SUCCESS_GET_TRACK_DETAILS is dispatched when the store call succeeds', () => {
+    getTrackDetailsGenerator = fetchTrackDetails({ trackId });
+    const res = getTrackDetailsGenerator.next().value;
+    expect(JSON.stringify(res)).toEqual(JSON.stringify(select(selectTrackById(trackId))));
+    // const trackDetailsResponse = {
+    //   resultCount: 1,
+    //   results: []
+    // };
+    // expect(getTrackDetailsGenerator.next(apiResponseGenerator(true, trackDetailsResponse)).value).toEqual(
+    //   put({
+    //     type: iTunesProviderTypes.SUCCESS_GET_TRACK_DETAILS,
+    //     data: trackDetailsResponse
+    //   })
+    // );
   });
 });
--- app/containers/ITunesProvider/tests/selectors.test.js
+++ app/containers/ITunesProvider/tests/selectors.test.js
@@ -1,16 +1,40 @@
-import { fromJS } from 'immutable';
-import { selectITunesProviderDomain } from '../selectors';
+import { selectITunesProviderDomain, selectTracksData, selectArtistName, selectTracksError } from '../selectors';
 
 describe('ITunesProvider selector tests', () => {
+  let artistName;
+  let tracks;
+  let tracksError;
   let mockedState;
 
   beforeEach(() => {
+    artistName = 'Opeth';
+    tracks = { resultCount: 0, results: [] };
+    tracksError = 'Internal Server Error';
     mockedState = {
-      iTunesProvider: fromJS({})
+      iTunesProvider: {
+        artistName,
+        tracks,
+        tracksError
+      }
     };
   });
 
-  it('should select the user state', () => {
+  it('should select the iTunesProvider state', () => {
     expect(selectITunesProviderDomain(mockedState)).toEqual(mockedState.iTunesProvider);
+  });
+
+  it('should select the artistName', () => {
+    const artistSelector = selectArtistName();
+    expect(artistSelector(mockedState)).toEqual(artistName);
+  });
+
+  it('should select the tracksData', () => {
+    const tracksSelector = selectTracksData();
+    expect(tracksSelector(mockedState)).toEqual(tracks);
+  });
+
+  it('should select the tracksError', () => {
+    const errorSelector = selectTracksError();
+    expect(errorSelector(mockedState)).toEqual(tracksError);
   });
 });
